f9c178ad8a26aaf871668638b483960e2db7f915
Francois Laberge <francois@jibo.com>
2015-10-12 00:20:27 -0400

    Fix for working in the main process

--- src/lifesized.js
+++ src/lifesized.js
@@ -3,7 +3,6 @@
  */
 
 var screen = require('screen'),
-    remote = require('remote'),
     execSync = require('child_process').execSync,
     path = require('path'),
     os = require('os');
@@ -16,9 +15,9 @@ var lifesized = {
      * @param [screen] (Electron Screen Instance) If not provided the display
               this process' BrowserWindow' is over will be used
      */
-    ppi: function(screen){
-        screen = screen || getBrowserWindowDisplay();
-        return getPPI(screen);
+    ppi: function(display){
+        display = display || getBrowserWindowDisplay();
+        return getPPI(display);
     }
 };
 
@@ -26,19 +25,22 @@ var lifesized = {
  * Get the display that the window is mostly over
  */
 function getBrowserWindowDisplay(){
+    // Don't require remote unless we hit here as it can't be required in the
+    // main process. Main processes need to provide
+    var remote = require('remote');
     return screen.getDisplayMatching(remote.getCurrentWindow().getBounds());
 }
 
 /**
  * Get a screens PPI
  */
-function getPPI(screen){
+function getPPI(display){
     // If on a mac use our native binary to do the work, otherwise fall back
     // to a default
     if(os.platform() === "darwin"){
         return  parseFloat(
                     execSync(
-                        path.resolve(__dirname, "../bin/lifesized-cli") + " " + screen.id
+                        path.resolve(__dirname, "../bin/lifesized-cli") + " " + display.id
                     )
                 );
     } else {
