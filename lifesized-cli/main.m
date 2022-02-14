469d368409d3e6b46e041fdb35a4e93849464028
Francois Laberge <francoislaberge@gmail.com>
2022-02-14 10:48:44 -0500

    Version of app that just logs out the information about each display

--- lifesized-cli/main.m
+++ lifesized-cli/main.m
@@ -12,13 +12,6 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
-        // Make sure there was a parameter passed
-        if(argc != 2){
-            printf("lifesized-cli requires a screenId as it's first argument");
-            return -1;
-        }
-
-        long targetScreenId = atoi(argv[1]);
         long count = [[NSScreen screens] count];
         
         // Find the screen with the same screen ID
@@ -26,32 +19,16 @@ int main(int argc, const char * argv[]) {
             NSScreen *screen = [[NSScreen screens] objectAtIndex:i];
             NSDictionary *description = [screen deviceDescription];
             NSNumber* screenId = [description objectForKey:@"NSScreenNumber"];
+          
+            printf("%0.0f (devicePixelRatio)\n", [screen backingScaleFactor]);
+          
+            NSSize displayPixelSize = [[description objectForKey:NSDeviceSize] sizeValue];
+            CGSize displayPhysicalSize = CGDisplayScreenSize([screenId unsignedIntValue]);
             
-            if([screenId longValue] == targetScreenId){
-
-                NSSize displayPixelSize = [[description objectForKey:NSDeviceSize] sizeValue];
-                
-                CGSize displayPhysicalSize = CGDisplayScreenSize(
-                                                                 [screenId unsignedIntValue]);
-                
-                // There are 25.4 millimeters per inch:
-                // https://www.google.com/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q=mm+to+inches
-                printf("%0.2f",(displayPixelSize.width / displayPhysicalSize.width) * 25.4f);
-
-                // Exit successfully
-                return 0;
-            }
+            // There are 25.4 millimeters per inch:
+            // https://www.google.com/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q=mm+to+inches
+            printf("%0.1f ppi\n",(displayPixelSize.width / displayPhysicalSize.width) * 25.4f * [screen backingScaleFactor]);
         }
-        
-        // If we reached here we might have had some race condition where a display went away in since
-        // when the call to this command line binary was made
-        printf("null");
-        return -1;
-
-
-        // there being 25.4 mm in an inch
-        
-        //NSLog(@"Hello, World!");
     }
     return 0;
 }
